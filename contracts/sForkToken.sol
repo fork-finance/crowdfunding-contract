658c47a2de6e699213b65896ddaadd11602e5b63
jerrkill <2273716951@qq.com>
2021-03-29 01:37:09 +0800

    perf: remove sFork's cap limit

--- contracts/sForkToken.sol
+++ contracts/sForkToken.sol
@@ -5,21 +5,21 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // sForkToken.
-contract sForkToken is ERC20("Share ForkToken", "sFORK"), Ownable {
+contract sForkToken is ERC20("Share Fork Token", "sFORK"), Ownable {
 
   // 18800000*0.1
-  uint256 private _cap = 1880000e18;
+  // uint256 private _cap = 1880000e18;
 
   constructor() public {
     _setupDecimals(18);
   }
 
-  function cap() public view returns (uint256) {
-    return _cap;
-  }
+  // function cap() public view returns (uint256) {
+  //   return _cap;
+  // }
 
   function mint(address _to, uint256 _amount) public onlyOwner {
-    require(totalSupply().add(_amount) <= cap(), "cap exceeded");
+    // require(totalSupply().add(_amount) <= cap(), "cap exceeded");
     _mint(_to, _amount);
   }
 
